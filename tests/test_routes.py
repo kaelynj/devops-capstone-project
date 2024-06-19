1f748cbf1af7107d51ca2633b96838a57331c272
kaelynj <43348706+kaelynj@users.noreply.github.com>
2024-06-19 09:41:40 -0400

    Implement update account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -150,3 +150,30 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should Update an Account"""
+        test_account = AccountFactory()
+        resp = self.client.post(
+            BASE_URL,
+            json=test_account.serialize()
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(
+            f"{BASE_URL}/{new_account['id']}", 
+            json = new_account
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account['name'], 'Something Known')
+
+    def test_update_an_account_not_found(self):
+        """It should Not Update an Account that is not found"""
+        fake_id = 0
+        test_account = AccountFactory()
+        resp = self.client.put(
+            f"{BASE_URL}/{fake_id}"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
